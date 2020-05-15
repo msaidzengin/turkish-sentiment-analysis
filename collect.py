b29d965b1f2861feb989dc3fa2c5befa8529f5f3
msaidzengin <m.zengin@etu.edu.tr>
2020-05-15 09:41:26 +0300

    change

--- collect.py
+++ collect.py
@@ -3,25 +3,24 @@ import twint
 pozitive = [":)","<3","😀","😃","😄","😁","😆","😅","😂","🤣","☺️","😊","😇","🙂","🙃","😍","🤓","😎","👏","👍","🙏","❤️"]
 negative = [":(",":/","</3","😞","😔","😟","😕","🙁","☹️","😣","😖","😫","😩","😢","😭","😤","😠","😡","🤬","😥","😓","🤒"]
 
-print(len(pozitive))
-print(len(negative))
-exit(0)
 c = twint.Config()
 c.Lang = "tr"
 c.Limit = 10
-c.Store_json = True
-c.Custom["tweet"] = ["id", "tweet"]
-c.Output = "pozitive.json"
+c.Store_csv = True
+c.Custom["tweet"] = ["tweet"]
 c.Hide_output = True
 
-
-"""
+count = 1
 for p in pozitive:
+    print(p)
+    c.Output = "p" + str(count) +".txt"
     c.Search = p
     twint.run.Search(c)
-"""
+    count += 1
 
-c.Output = "negative.json"
+count = 1
 for n in negative:
+    print(p)
+    c.Output = "n" + str(count) +".txt"
     c.Search = n
     twint.run.Search(c)
