a9a93ca65aadfd8fafc15707310c173f5fda6654
MuhammedSaid ZENGİN <muhammedsaid.zengin@btk.gov.tr>
2020-07-01 11:43:40 +0300

    data cleaning

--- clean.py
+++ clean.py
@@ -1,14 +1,17 @@
 import string
 import re
 
-with open("dataset/negative_sentences.txt", encoding="utf-8") as f:
+with open("dataset/positive_sentences.txt", encoding="utf-8") as f:
     content = f.readlines()
 content = [x.strip() for x in content] 
 
 whitelist = set('abcçdefgğhıijklmnoöprsştuüvwxyz ')
 
-f = open('dataset/clean_negative_sentences.txt', 'w', encoding='utf-8')
+f = open('dataset/clean_positive_sentences.txt', 'w', encoding='utf-8')
+count = 1
 for text in content:
+    print(count)
+    count += 1
     text = text.translate(str.maketrans('', '', string.punctuation)).lower()
     text = ''.join(filter(whitelist.__contains__, text))
     text = ' '.join(filter(lambda x:x[:10]!='pictwitter' and x[:4]!='http', text.split()))
